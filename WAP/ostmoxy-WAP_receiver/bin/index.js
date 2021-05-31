1b507f61a6c61fdbccdadffa892f65324ba372ea
Chris Ostmo <chris@missionlab.dev>
2021-05-30 23:10:07 -0700

    Fully functioning WPA supplicant file value replacer for WAP functionality

--- WAP/ostmoxy-WAP_receiver/bin/index.js
+++ WAP/ostmoxy-WAP_receiver/bin/index.js
@@ -1,21 +1,145 @@
 #!/usr/bin/env node
 
-require( 'strict-mode' )(function () {
+/**
+ * Standalone script to read serial input from an ESP8266-12E over the serial (TX/RX)
+ *   pins and set the server's WiFi credentials accordng to the input
+ *
+ * Must be run as root
+ */
 
-	const raspi = require('raspi');
-	const Serial = require('raspi-serial').Serial;
+ require( 'strict-mode' )(function () {
 
-	parseInput = ( {input = null} ) => {
-		// console.log( "GOT INPUT: " + input );
-		let jsonData = JSON.parse( input.toString() );
-		console.log( "GOT DATA: " );
-		// console.log( "ACTION : " + jsonData.action );
-		// console.log( "SSID   : " + jsonData.ssid );
-		// console.log( "PASS   : " + jsonData.password );
+	const { exec } = require( 'child_process' );
+	const raspi = require( 'raspi' );
+	const Serial = require( 'raspi-serial' ).Serial;
+	const shell = require( 'shelljs' );
+	const fs = require( 'fs' );
+	const dateFormat = require( 'dateformat' );
 
-		// TODO: Write the values to /boot/octopi-wpa-supplicant.txt
-		// TODO: Reboot the rPi
+	const baseDir = "/home/pi/.ostmoxy";
+	const backupDir = baseDir + "/backups";
+	const wpaInputFile = "/boot/octopi-wpa-supplicant.txt";
+
+	// Require root user before continuing
+	if( process.env.USER != "root" ) {
+		console.log( "This script must be run as root." );
+		process.exit( 0 );
 	}
+
+	/**
+	 * Write a new WPA supplicant file before rebooting
+	 *
+	 * @param JSON	jsonData			Incoming data object
+	 */
+	writeWpaSupplicantFile =
+	( {jsonData = null} ) =>
+	{
+		return new Promise(
+			(resolve, reject) =>
+			{
+				let now = new Date();
+				let fileDate = dateFormat( now, "yyyymmdd-HHMMss" );
+				let outputFile = backupDir + "/octopi-wpa-supplicant.txt." + fileDate;
+
+				shell.cp( wpaInputFile, outputFile );
+				let wpaContent = "";
+
+				// Write the values to /boot/octopi-wpa-supplicant.txt
+				if( jsonData && jsonData.password && jsonData.password.length > 0 )
+				{
+					wpaContent =
+					"## WPA/WPA2 secured\n" +
+					"network={\n" +
+					"  ssid=\"" + jsonData.ssid + "\"\n" +
+					"  psk=\"" + jsonData.password + "\"\n" +
+					"}\n";
+				} else {
+					wpaContent =
+					"## Open/unsecured\n" +
+					"network={\n" +
+					"  ssid=\"" + jsonData.ssid + "\"\n" +
+					"  key_mgmt=NONE\n" +
+					"}\n";
+				}
+
+				wpaContent +=
+				"country=US # United States\n" +
+				"ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev\n" +
+				"update_config=1";
+
+				fs.writeFileSync( wpaInputFile, wpaContent );
+				return resolve( true );
+			}
+		);
+	}
+
+	/**
+	 * Reboot the rPi
+	 */
+	reboot =
+	() => {
+		exec( "shutdown -r now", ( error, stdout, stderr ) =>{} );
+	}
+
+	/**
+	 * Parses incoming data and send readable results to a handler
+	 */
+	parseInput =
+	( {input = null} ) => {
+
+		try
+		{
+			// Sanity check the input
+			let jsonData = JSON.parse( input.toString() );
+			if( !jsonData || !jsonData.action || !jsonData.ssid )
+			{
+				console.log( "CAUGHT ERROR" );
+				console.log( error );
+				console.log( jsonData );
+				return;
+			}
+			// Set WiFi on request
+			else if( jsonData.action == "set-wifi" )
+			{
+				writeWpaSupplicantFile( {jsonData: jsonData} )
+					.then(
+						(supplicantResponse) => {
+							// Reboot the rPi after a short pause to make sure IO is complete
+							setTimeout(
+								() =>
+								{
+									reboot();
+									return;
+								}, 5000
+							);
+						}
+					 );
+
+			} else {
+
+				console.log( "NO DEFINED HANDLER FOR INPUT" );
+				console.log( jsonData );
+			}
+		}
+		catch( error )
+		{
+			console.log( "CAUGHT ERROR" );
+			console.log( error );
+			return;
+		}
+	}
+
+	// Make sure everything's setup
+	init = () =>
+	{
+		if( !fs.existsSync( backupDir ) )
+		{
+			shell.mkdir( "-p", backupDir );
+		}
+	}
+	init();
+
+	// Ingest some data...
 
 	// An incoming string buffer
 	let incoming = "";
