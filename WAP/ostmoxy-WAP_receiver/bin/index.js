699e8dfa898aa4acdde5837e1266e7543c1c81fa
Chris Ostmo <chris@missionlab.dev>
2021-05-30 18:01:32 -0700

    Functioning serial data receiver for the rPi in NodeJS

--- WAP/ostmoxy-WAP_receiver/bin/index.js
+++ WAP/ostmoxy-WAP_receiver/bin/index.js
@@ -5,17 +5,35 @@ require( 'strict-mode' )(function () {
 	const raspi = require('raspi');
 	const Serial = require('raspi-serial').Serial;
 
+	parseInput = ( {input = null} ) => {
+		// console.log( "GOT INPUT: " + input );
+		let jsonData = JSON.parse( input.toString() );
+		console.log( "GOT DATA: " );
+		// console.log( "ACTION : " + jsonData.action );
+		// console.log( "SSID   : " + jsonData.ssid );
+		// console.log( "PASS   : " + jsonData.password );
+
+		// TODO: Write the values to /boot/octopi-wpa-supplicant.txt
+		// TODO: Reboot the rPi
+	}
+
+	// An incoming string buffer
+	let incoming = "";
+	// Listen to the serial port
 	raspi.init(
 		() => {
 			let serial = new Serial( {portId: '/dev/serial0', baudRate: 57600} );
 			serial.open(
 				() => {
-					serial.on('data', (data) => {
-
-						process.stdout.write( data );
-
-					});
-					// serial.write('Hello from raspi-serial');
+					serial.on( 'data',
+						(data) => {
+							// Don't send for processing until we have end of line
+							incoming += data;
+							if( data.toString().endsWith( "\n" ) ) {
+								parseInput( {input: incoming} );
+								incoming = "";
+							}
+						});
 				});
 		});
 
